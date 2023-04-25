a5d344fca723bc6f71fa0bf72e4475ad7e671524
draginojd <arminfazlikhan@gmail.com>
2023-04-25 04:07:12 +0200

    fill credentials scenario added

--- specs/add-to-cart.feature
+++ specs/add-to-cart.feature
@@ -1,7 +1,32 @@
-Feature: Add to cart  
+Feature: Cart  
 
 Scenario: Add a product to cart
   Given  that Im on the start page
   And  entered the letter "t"
   When  I click on buy button of the first product
   Then "1" item should be added to cart
+
+Scenario: Go to checkout
+  Given that Im on the start page
+   And  entered the letter "t"
+  When  I click on buy button of the first product
+  Then "1" item should be added to cart
+  And I click on go to Cart
+
+Scenario: Fill in credentials
+  Given that Im on the start page
+  And entered the letter "t"
+  When I click on buy button of the first product
+  Then "1" item should be added to cart
+  Given I click on go to Cart
+  Then I enter "Peterpan Gustavsson" as my name
+  And I enter "Fifth Avenue" as my home adress
+  And I enter "402 25" as my postal code
+  And I enter "New York City" as my state
+  And I enter "403 1525 3525 2535" Card Number as my card Number
+  And I enter "03/26" Date of validation
+  And I enter "979" as my CCV code
+
+  
+
+  
--- specs/step_definitions/add-to-cart.js
+++ specs/step_definitions/add-to-cart.js
@@ -15,5 +15,37 @@ When('I click on buy button of the first product', () => {
 Then('{string} item should be added to cart', (amount) => {
   cy.get("#cart li")
     .contains(amount + "st")
-    .should("exist")
+    .should("exist");
+});
+
+Given('I click on go to Cart', () => {
+  cy.get("#cart button").click();
+});
+
+Then('I enter {string} as my name',() => {
+  cy.get("#name").click().type("Peterpan Gustavsson");
+});
+
+Then('I enter {string} as my home adress',  () => {
+  cy.get("#street").click().type("Fifth Avenue");
+});
+
+Then('I enter {string} as my postal code', () => {
+  cy.get("#zip").click().type("402 25");
+});
+
+Then('I enter {string} as my state', () => {
+  cy.get("#city").click().type("New York City");
+});
+
+Then('I enter {string} Card Number as my card Number', () => {
+  cy.get("#card-nr").click().type("403 1525 3525 2535");
+});
+
+Then('I enter {string} Date of validation', () => {
+  cy.get("#card-date").click().type("03/26");
+});
+
+Then('I enter {string} as my CCV code', () => {
+  cy.get("#card-ccv").click().type("979");
 });
